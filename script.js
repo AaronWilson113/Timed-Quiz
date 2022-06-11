641c7e3f0d51ad6c7d1d07b10e437535010db16c
President <aaron.m.wilson@uconn.edu>
2022-06-11 16:49:04 -0400

    changed html and css slightly so i could select welcome page elements easier and organize them at the top of my javascript page

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var body = document.body;
-//var to create my question element
-var questionAsked = document.querySelector("#title");
-//var for my start button
-var startQuiz = document.querySelector("#start-quiz");
 
+// selecting welcome page elements 
+var startQuizBtn = document.querySelector("#start-quiz");
+var welcomePageEl = document.querySelector("#welcome-page")
 
 
 
 //addEvent listener to render first question to the page 
-startQuiz.addEventListener("click" , beginTheQuiz)
+startQuizBtn.addEventListener("click" , beginTheQuiz)
+
+
 
 //function to start the quiz
 function beginTheQuiz(array) {
+    alert("asodhaahoudasdadasda")
     
 }
 
