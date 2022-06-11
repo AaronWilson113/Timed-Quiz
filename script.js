c2b8e7fa167b96758986a171e3fcd967c9ebfd96
President <aaron.m.wilson@uconn.edu>
2022-06-11 16:30:36 -0400

    removed array for questions

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 var body = document.body;
-//array for questions
-var questionsArray = ["question 1 placeholder" , "question 2 placeholder" , "question 3 placeholder" , "question 4 placeholder" , "question 5 placeholder"];
 //var to create my question element
 var questionAsked = document.querySelector("#title");
 //var for my start button
@@ -14,13 +12,7 @@ startQuiz.addEventListener("click" , beginTheQuiz)
 
 //function to start the quiz
 function beginTheQuiz(array) {
-
-    var randomQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
-    questionAsked.appendChild(randomQuestion);
-
     
 }
 
 
-//all created element styles
-questionAsked.setAttribute("style" , "background:888888")
