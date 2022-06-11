337a37fff2985bfa2de71f93fc2e57b87ea0a68d
President <aaron.m.wilson@uconn.edu>
2022-06-11 13:46:57 -0400

    added function in javascript as well as add event listener button and input placeholder texxt

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-//arrray of questions 
-//array of answers to the questions, linked somehow to the array of questions?
-// create a button to click that starts the quiz
-//once button is clicked, start the timer, call function to render my first question
-//for my questions, need to be text objects w four buttons
-//when question is answered right add points to score and render next question,
-//if answer is wrong remove time from timer and render next question
-//when game ends, stop timer and render the score to screen with and option to add initials and store high score
-
-
+var body = document.body;
+//var to create my question element
+var questionAsked = document.createElement("h1");
 //var for my start button
 var startQuiz = document.querySelector("#start-quiz");
+//temporary text content 
+questionAsked.textContent = "aodhosaudabdoadad"
 
 
 
 //addEvent listener to render first question to the page 
 startQuiz.addEventListener("click" , beginTheQuiz)
 
+//function to start the quiz
 function beginTheQuiz() {
-    enter = parseInt(prompt("Choose a number, any number, between 8 and 128"));
+
+    body.appendChild(questionAsked);
+    
 }
+
+
+//all created element styles
+questionAsked.setAttribute("style" , "background:888888")
