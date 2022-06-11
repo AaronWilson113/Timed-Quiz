196579853fe40a51984fda59c68fbf17e1275727
President <aaron.m.wilson@uconn.edu>
2022-06-11 17:09:45 -0400

    added timer elements to javascript page

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ var body = document.body;
 // selecting welcome page elements 
 var startQuizBtn = document.querySelector("#start-quiz");
 var welcomePageEl = document.querySelector("#welcome-page")
+//selecting header / timer elements 
+var timerEl = document.querySelector("#timer")
+var timerTextEl = document.querySelector("#timer-text")
 
 
 
-//addEvent listener to render first question to the page 
+//addEvent listener to start the quiz
 startQuizBtn.addEventListener("click" , beginTheQuiz)
 
 
 
 //function to start the quiz
-function beginTheQuiz(array) {
-    alert("asodhaahoudasdadasda")
-    
+function beginTheQuiz() {
+
+
+    startTimer()
 }
 
+//start timer function
+function startTimer() {
+    timer = setInterval(function() {
+
+
+    })
+}
 
