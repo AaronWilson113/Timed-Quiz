b76ae9cef5ecb687b62300e3b194f4631505293e
President <aaron.m.wilson@uconn.edu>
2022-06-11 04:28:31 -0400

    added pseudo code to javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+//arrray of questions 
+//array of answers to the questions, linked somehow to the array of questions?
+// create a button to click that starts the quiz
+//once button is clicked, start the timer, call function to render my first question
+//for my questions, need to be text objects w four buttons
+//when question is answered right add points to score and render next question,
+//if answer is wrong remove time from timer and render next question
+//when game ends, stop timer and render the score to screen with and option to add initials and store high score
